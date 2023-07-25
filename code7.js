77c125baad18cd28b90e76c6e0d23a501f6e2096
Daniel Leal <daniel.suportte@gmail.com>
2023-07-24 23:59:30 -0300

    Código exercício code7

---
+++ code7.js
@@ -0,0 +1,25 @@
+const prompt = require("prompt-sync")();
+
+function tabuada() {
+    // Solicita ao usuário que digite um número inteiro
+    const numero = parseInt(prompt("Digite um número inteiro para ver a sua tabuada: "));
+
+    // Verifica se o valor digitado é um número válido (isNan = para testar se não é um número. Se não for um número válido, uma mensagem de erro é exibida.)
+    if (isNaN(numero)) {
+        console.log("Por favor, digite um número válido!");
+        return tabuada();
+    } else {
+        //Imprime a tabuada do número digitado
+        console.log("Tabuada do " + numero + ":");
+    
+
+    for (let i = 0; i <= 10; i++) {
+        const resultado = numero * i;
+        console.log(numero + " x " + i + " = " + resultado);
+        
+    }
+}
+    
+}
+
+tabuada();
