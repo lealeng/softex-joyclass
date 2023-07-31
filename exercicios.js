9f9d2fd8118e68d41b5ff98f6f16f468b20e3f66
Daniel Leal <daniel.suportte@gmail.com>
2023-07-31 17:15:35 -0300

    adicionando exercicio contagem bomba

--- exercicios.js
+++ exercicios.js
@@ -14,4 +14,21 @@ function aprovacaoAluno() {
   }
 }
 
-aprovacaoAluno();
+// Faça um código, utilizando o Google Blockly, que execute a contagem regressiva de uma bomba,
+// informando o número de segundos para explodir. Ele deverá mostrar a mensagem
+// “iniciando contagem regressiva”, os segundos passados e, no final, a mensagem “BUM!”.
+
+function contagem() {
+  const segundos = 4;
+  console.log("Iniciando contagem regressiva . . . ");
+
+  for (let i = segundos; i >= 0; i--) {
+    if (i > 0) {
+      console.log(i + " segundos passados . . .");
+    } else {
+      console.log("BUM!");
+    }
+  }
+}
+
+
