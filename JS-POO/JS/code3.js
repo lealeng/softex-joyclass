0f9e4aff2c27b59f67f0b8ecd2ca724ac34f8e76
Daniel Leal <daniel.suportte@gmail.com>
2023-08-21 21:54:46 -0300

    code3

---
+++ JS-POO/JS/code3.js
@@ -0,0 +1,31 @@
+// CODE 3
+function UNDEFINED() {
+  let boolean;
+  let bigInt;
+  let string;
+  let symbol;
+
+  console.log(boolean, bigInt, string, symbol);
+}
+
+// NULL
+function NULL() {
+  let boolean = null;
+  let bigInt = null;
+  let string = null;
+  let symbol = null;
+
+  console.log(boolean, bigInt, string, symbol);
+}
+
+function VALORES() {
+  let boolean = true;
+  let bigInt = 1524578755787825145;
+  let string = "null";
+  let symbol = {};
+
+  console.log(boolean, bigInt, string, symbol);
+}
+UNDEFINED();
+NULL();
+VALORES();
