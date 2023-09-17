18a5f0cf772baa88aa0983381dd6ed148c6f203f
Daniel Leal <daniel.suportte@gmail.com>
2023-09-17 11:25:56 -0300

    code9

---
+++ JS-POO/JS/code9.js
@@ -0,0 +1,17 @@
+function Function() {
+    
+}
+
+function somar(a, b) {
+    return a + b;
+    
+}
+
+const somar = (a, b) => a + b;
+
+
+const calc = (nota1, nota2, resultado) => {
+   resultado = nota1 + nota2
+   return resultado;
+}
+
